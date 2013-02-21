bc51ea485ee81e98d80d03c9c5f1b6b19f7b193e
Michael Gilliland <mjg.py3@gmail.com>
2013-02-21 00:39:53 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+MJG server
+----------
+
+Simply a fun server I am building to play around with RoR and some other languages
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MjgServer::Application.config.secret_token = 'ae47b29e69d330b74ea53256bfd6a1d55681c9bb6282cb1253684da2d57bd0a3a40790a781b307bd76e47edead0f9cd9b968c23498e6624a3a61b5640fbddb81'
