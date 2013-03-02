6b0442127f933c6117f0be6abd49410a0a4cc5d4
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-01 21:09:23 -0500

    Finished a _working_ LyricThief addition

--- app/controllers/lyric_thief_controller.rb
+++ app/controllers/lyric_thief_controller.rb
@@ -1,7 +1,31 @@
+require 'socket'
+
 class LyricThiefController < ApplicationController
   def index
   end
 
   def display
+    @artist = params[:artist]
+    @song = params[:song_name]
+
+    host = Socket.gethostname
+    port = 915
+    @result = ''
+
+    begin
+      s = TCPSocket.new host, port
+    rescue
+    end
+
+    s.puts("{ \"artist\" : \"#{@artist}\", \"song\" : \"#{@song}\"}")
+
+    begin
+      while line = s.gets   # Read lines from the socket
+       @result +=  line.chop + "\n"      # And print with platform line terminator
+      end
+    rescue
+    end
+    s.close               # Close the socket when done
+
   end
 end
