c94c38978f89763420b004dd2beeb1b8aac1920b
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-06 03:47:31 -0500

    Added a title making ruby helper for lyric_thief

--- app/helpers/lyric_thief_helper.rb
+++ app/helpers/lyric_thief_helper.rb
@@ -1,2 +1,19 @@
 module LyricThiefHelper
+  def title_ize name
+
+    not_allowed = ['for', 'and', 'nor', 'but',
+                   'or', 'yet', 'so', 'in', 'to',
+                   'over', 'an', 'a', 'the']
+
+    result = []
+
+    name.split(' ').each() do | word |
+      result << word if not_allowed.include? word
+      result << word.capitalize if not not_allowed.include? word
+    end
+
+    result[0] = result[0].capitalize
+
+    result.join ' '
+  end
 end
