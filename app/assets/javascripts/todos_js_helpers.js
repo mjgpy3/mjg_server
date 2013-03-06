31265d2d99c8228391fad90a4da90a3103166e5a
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-06 11:09:58 -0500

    Added a color selector JS method and some HTML to make it work for todos

--- app/assets/javascripts/todos_js_helpers.js
+++ app/assets/javascripts/todos_js_helpers.js
@@ -4,3 +4,9 @@ function box(element) {
 function unbox(element) {
   element.style.border = "";
 }
+function color_selected(element) {
+  color = element.style.backgroundColor;
+  text_field = document.getElementById("todo_color");
+
+  text_field.value = color;
+}
