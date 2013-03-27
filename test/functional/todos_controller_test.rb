e4b17a25c669c49f7380732cf905d96754125b37
Michael J. Gilliland <mjg.py3@gmail.com>
2013-03-27 09:41:01 -0400

    Wrote my first RoR test code.

--- test/functional/todos_controller_test.rb
+++ test/functional/todos_controller_test.rb
@@ -5,4 +5,13 @@ class TodosControllerTest < ActionController::TestCase
     @todo = todos(:one)
   end
 
+  test "When we destroy a todo we should see one less in the model" do
+    destroy_me = todos(:three)
+    n_todos_before_destroy = Todo.count
+
+    Todo.destroy destroy_me
+
+    assert (Todo.count == n_todos_before_destroy-1), "Did not destroy a todo successfully"
+  end
+
 end
